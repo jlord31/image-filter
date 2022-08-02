2b69310329d04625b2b7084a9dc72ab60fa47740
Joseph <Adepoju97@gmail.com>
2022-08-02 23:30:03 +0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,84 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles, isValidUrl} from './util/util';
+
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8080;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // app.get("/filteredimage?image_url={{}}",async (req, res) => {
+  //   // let { url } = req.url.split("?");
+  //   // return url;
+  //   //const url = window.location.search.substring(1);
+  //   const param = new URLSearchParams(window.location.search);
+  //   const url = param.get("image_url");
+
+  //   return url;
+      
+  // } );
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+  // End point for image filtering
+  app.get("/filteredimage",async (req, res) => {
+
+    try {
+      const parameters = req.query.image_url;
+
+      if(!parameters){
+        res.send({error: "Please include image_url and the url to the image in the query parameters"});
+      }
+
+      else if(!isValidUrl(parameters)){
+        res.send({error: "Invalid image URL"});
+      }
+      else{
+        const filteredpath = await filterImageFromURL(parameters);
+      
+        res.sendFile(filteredpath);
+        res.on('finish', () => deleteLocalFiles([filteredpath]));
+      }
+      
+      
+    } catch (error) {
+      res.status(500).send({error: 'Unable to process your request'});
+    }
+    
+  });
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
